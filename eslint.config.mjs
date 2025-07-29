fedcab603b181e90e344535f41aa31bc7aa9c70e
Rimao416 <kayumbaomari27@gmail.com>
2025-07-29 13:02:26 +0200

    :recycle: Mise à jour du fichier esconfig

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      "src/generated/**/*",
+      "**/generated/**/*",
+      "**/*.generated.*",
+      ".next/**/*",
+      "node_modules/**/*"
+    ]
+  }
 ];
 
 export default eslintConfig;
